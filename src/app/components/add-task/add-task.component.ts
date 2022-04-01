9b69acb32e621eb7e3e5caf4a08c731b88188c83
foly13 <foly_13@hotmail.com>
2022-04-01 19:11:35 -0300

    task list terminanda

---
+++ src/app/components/add-task/add-task.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UiServiceService } from 'src/app/service/ui-service.service';
+import { Task } from '../../Task';
+
+@Component({
+  selector: 'app-add-task',
+  templateUrl: './add-task.component.html',
+  styleUrls: ['./add-task.component.css']
+})
+export class AddTaskComponent implements OnInit {
+  @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
+  text: string=""
+  day: string=""
+  reminder: boolean= false;
+  showAddTask: boolean = false;
+  subscription?: Subscription;
+
+  constructor( private uiService: UiServiceService) {
+    this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value)
+   }
+
+  ngOnInit(): void {
+  }
+  onSubmit(){
+    if(this.text.length === 0){
+      alert("Please add a task!");
+      return
+    }
+    const {text, day, reminder} = this
+    const newTask = {text,day,reminder}
+    this.onAddTask.emit(newTask);
+  }
+
+}
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UiServiceService } from 'src/app/service/ui-service.service';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,12 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   title:string = 'My Task List';
+  showAddTask: boolean = false;
+  subscription?:Subscription;
 
-  constructor() { }
+  constructor(private uiService: UiServiceService, private router: Router) {
+    this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value)
+   }
 
   ngOnInit(): void {
   }
 toggleAddTask(){
-  console.log("toggleAddTask!");
+  this.uiService.toggleAddTask();
+}
+hasRoute(route:string){
+  return this.router.url === route
 }
 }
